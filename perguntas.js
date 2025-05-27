4b76255bdd2610be228df8f4b5521e4116e52fd3
guembar856 <mariana.ferreira.franca@escola.pr.gov.br>
2025-05-27 20:07:20 +0000

    Aula6

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Charadas',
+    'O que é, o que é? Dois pontinhos cinzas no oceano?',
+    'Twobarões'
+)
+
+criaCartao(
+    'Charadas',
+    'O que é, o que é? Um pontinho preto no avião?',
+    'Uma aeromosca'
+)
+
+criaCartao(
+    'Charadas',
+    'O que é, o que é? Um pontinho vermelho n parede?',
+    'Uma joaninha de castigo'
+)
